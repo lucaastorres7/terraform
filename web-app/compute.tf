d98cb17468c550df9706b86e9147c66fdac4d11d
Lucas Torres <lucastorreslr10@gmail.com>
2026-08-20 13:39:13 -0300

    feat: add missing outbound rules and nsg allowing traffic from lb to instance

--- web-app/compute.tf
+++ web-app/compute.tf
@@ -2,7 +2,7 @@
 resource "aws_instance" "web-app-1" {
   ami             = "ami-0332d564d76dbd8d6" # Amazon Linux 2023 kernel-6.18
   instance_type   = "t3.micro"
-  security_groups = [aws_security_group.web-app-sg.name]
+  security_groups = [aws_security_group.web-app-sg.name, aws_security_group.lb-to-instance-sg.name]
   user_data       = file("./files/ec2_user_data1.sh")
 
   tags = {
@@ -13,7 +13,7 @@ resource "aws_instance" "web-app-1" {
 resource "aws_instance" "web-app-2" {
   ami             = "ami-0332d564d76dbd8d6" # Amazon Linux 2023 kernel-6.18
   instance_type   = "t3.micro"
-  security_groups = [aws_security_group.web-app-sg.name]
+  security_groups = [aws_security_group.web-app-sg.name, aws_security_group.lb-to-instance-sg.name]
   user_data       = file("./files/ec2_user_data2.sh")
 
   tags = {
--- web-app/networking.tf
+++ web-app/networking.tf
@@ -13,6 +13,11 @@ resource "aws_security_group" "web-lb-sg" {
   description = "Security Group for allowing internet traffic to multiple web servers"
 }
 
+resource "aws_security_group" "lb-to-instance-sg" {
+  name        = "lb-to-instance-sg"
+  description = "Security Group for allowing traffic between ALB and Instances"
+}
+
 # SG Rules
 resource "aws_vpc_security_group_ingress_rule" "web-app-allow" {
   security_group_id = aws_security_group.web-app-sg.id
@@ -39,6 +44,27 @@ resource "aws_vpc_security_group_ingress_rule" "web-lb-allow-https" {
   from_port   = 443
   to_port     = 443
   ip_protocol = "tcp"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "lb-to-instance-allow" {
+  security_group_id = aws_security_group.lb-to-instance-sg.id
+
+  from_port                    = 8080
+  to_port                      = 8080
+  ip_protocol                  = "tcp"
+  referenced_security_group_id = aws_security_group.web-lb-sg.id
+}
+
+resource "aws_vpc_security_group_egress_rule" "web-app-allow-all-out" {
+  security_group_id = aws_security_group.web-app-sg.id
+  cidr_ipv4 = "0.0.0.0/0"
+  ip_protocol = "-1"
+}
+
+resource "aws_vpc_security_group_egress_rule" "lb-allow-all-out" {
+  security_group_id = aws_security_group.web-lb-sg.id
+  cidr_ipv4 = "0.0.0.0/0"
+  ip_protocol = "-1"
 }
 
 # -- Data --
