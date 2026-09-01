4581ddcc8e45e5b2e5a32d3d9abbc8d872f73ede
Lucas Torres <lucastorreslr10@gmail.com>
2026-09-01 16:50:57 -0300

    chore: re-add previous compute outputs

--- web-app/modules/compute/outputs.tf
+++ web-app/modules/compute/outputs.tf
@@ -1,3 +1,11 @@
+output "instance_ids" {
+  value = aws_instance.ec2_instance[*].id
+}
+
+output "instance_public_ips" {
+  value = aws_instance.ec2_instance[*].public_ip
+}
+
 output "instances" {
   value = [for instance in aws_instance.ec2_instance : {
     id         = instance.id
